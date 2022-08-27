ff0830e58841d510e64930992b385fc470996f47
unknown <steveweed1979@gmail.com>
2022-08-26 23:30:23 -0500

    Working on the delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,31 +1,41 @@
-const routes = require('express').Router();
+const router = require('express').Router();
 const fs = require('fs');
-const{ readFile, writeFile } = fs.promises
+const { readFile, writeFile } = fs.promises
 const { v4: uuidv4 } = require('uuid');
 
 
-routes.get('/notes', (req, res) => {
-    readFile('db/db.json').then(function(data){
-      const db = JSON.parse(data)
-      res.json(db)
-    })
-  }  
-  );
-  
-  routes.post('/notes', (req, res) => {
-    const { body } = req;
-    readFile('db/db.json').then(function(data){
-      const db = JSON.parse(data)
-      const newNote = {
-        title: body.title,
-        text: body.text,
-        id: uuidv4()
-      }
-      db.push(newNote);
-      writeFile('db/db.json', JSON.stringify(db))
-      res.json(db)
-  
-    })
-  });
-  
-  module.exports = routes;
+router.get('/notes', (req, res) => {
+  readFile('db/db.json').then(function (data) {
+    const db = JSON.parse(data)
+    res.json(db)
+  })
+}
+);
+
+router.post('/notes', (req, res) => {
+  const { body } = req;
+  readFile('db/db.json').then(function (data) {
+    const db = JSON.parse(data)
+    const newNote = {
+      title: body.title,
+      text: body.text,
+      id: uuidv4()
+    }
+    db.push(newNote);
+    writeFile('db/db.json', JSON.stringify(db))
+    res.json(db)
+
+  })
+});
+
+router.post('/notes/:id', (req, res) => {
+  const { body } = req;
+  readFile('db/db.json').then(function (data) {
+    const db = JSON.parse(data)
+    const result = db.filter(db.id)
+    writeFile('db/db.json', JSON.stringify(result))
+    res.json(result)
+  })
+});
+
+module.exports = router;
