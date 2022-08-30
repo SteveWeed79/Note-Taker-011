2585d81415d0662b93339a07d0a531a658be1600
unknown <steveweed1979@gmail.com>
2022-08-30 17:32:04 -0500

    Added comments to the notes.js file

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
+// sets up the boilerplate for the app using express, file system, uuid and fs.promises
 const router = require('express').Router();
 const fs = require('fs');
 const { readFile, writeFile } = fs.promises
 const { v4: uuidv4 } = require('uuid');
 
-
+// sets the notes to be read from the db to the index
 router.get('/notes', (req, res) => {
   readFile('db/db.json').then(function (data) {
     const db = JSON.parse(data)
@@ -12,6 +13,7 @@ router.get('/notes', (req, res) => {
 }
 );
 
+// Sets the new note into the db.js
 router.post('/notes', (req, res) => {
   const { body } = req;
   readFile('db/db.json').then(function (data) {
@@ -28,6 +30,8 @@ router.post('/notes', (req, res) => {
   })
 });
 
+
+// TODO:  Get the delete function working
 // router.delete('/notes/:id', (req, res) => {
 //   const { body } = req;
 //   readFile('db/db.json').then(function (data) {
